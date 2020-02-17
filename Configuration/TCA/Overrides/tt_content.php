5d418a73518968b5283d2e63eb640a292a500251
Benjamin Riezler <b.riezler@pixel-ink.de>
2020-02-17 13:10:51 +0100

    [FEATURE] Add content width, additional class and additional header format

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,3 +2,53 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
+
+// add custom field 'tx_ptm_iconbox_color' to tt_content
+$temporaryColumns = array(
+    'px_bootstrap_row' => array(
+        'exclude' => 1,
+        'label' => 'Laufweite Inhalt',
+        'config' => array(
+            'type' => 'select',
+            'items' => array(),
+            'size' => 1,
+            'maxitems' => 1,
+        )
+    ),
+    'px_header_class' => array(
+        'exclude' => 1,
+        'label' => 'Überschrift Optik',
+        'config' => array(
+            'type' => 'select',
+            'items' => array(),
+            'size' => 1,
+            'maxitems' => 1,
+        )
+    ),
+    'px_additional_css_class' => array(
+        'exclude' => 1,
+        'label' => 'Zusätzliche CSS Klassen',
+        'config' => array(
+            'type' => 'input',
+            'size' => 80
+        )
+    )
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+    'tt_content',
+    $temporaryColumns
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'tt_content',
+    'px_bootstrap_row',
+    '',
+    'after:layout'
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'tt_content',
+    'px_header_class',
+    '',
+    'after:header_layout'
+);
---
+++ ext_tables.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tt_content (
+    px_bootstrap_row varchar(255) DEFAULT '' NOT NULL,
+    px_header_class  varchar(255) DEFAULT '' NOT NULL,
+    px_additional_css_class text
+);
